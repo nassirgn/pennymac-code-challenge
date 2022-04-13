6ad7612e838fb103335495ad3b87baf6f7392714
mamadoudiallo <mamadoudiallo@costco.com>
2022-04-13 14:50:58 -0700

    adding background Image

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Box, Typography, styled  } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 import useGetRandomQuote from "../../hooks/useGetRandomQuote";
 
@@ -7,50 +7,58 @@ const Home = () => {
   const quote = useGetRandomQuote();
 
   return (
-    <React.Fragment>
+    <div>
       {quote.isLoading ? (
         <span>loading...</span>
       ) : quote.isError ? (
         <span>Ops something happened...</span>
       ) : (
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            
-          }}
-        >
+        <React.Fragment>
           <Box
-            component="div"
             sx={{
-              resize: 'both',
-              whiteSpace: "normal",
-              m: 4,
-              p: 1,
-              bgcolor: (theme) =>
-                theme.palette.mode === "dark" ? "#101010" : "grey.100",
-              color: (theme) =>
-                theme.palette.mode === "dark" ? "grey.300" : "grey.800",
-              border: "1px solid",
-              borderColor: (theme) =>
-                theme.palette.mode === "dark" ? "grey.800" : "grey.300",
-              borderRadius: 2,
-              fontSize: "0.875rem",
-              fontWeight: "700",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
             }}
           >
-            <Typography variant="h4" sx={{fontStyle: 'italic'}}>
-            {`"${quote.data.data.quote}"`}
-
-            </Typography>
-            <Typography variant="h4" sx={{textAlign: 'center', py: 2}}>
-              {`Author: "${quote.data.data.author}"`}
-            </Typography>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                backgroundImage: `url('https://images.unsplash.com/photo-1518756131217-31eb79b20e8f')`,
+                m: 2,
+                p: 1,
+              }}
+            >
+              <Box
+                component="div"
+                sx={{
+                  resize: "both",
+                  whiteSpace: "normal",
+                  m: 4,
+                  p: 1,
+                  bgcolor: "grey.100",
+                  color: "grey.800",
+                  border: "1px solid",
+                  borderColor: "grey.300",
+                  borderRadius: 2,
+                  fontSize: "0.875rem",
+                  fontWeight: "700",
+                }}
+              >
+                <Typography variant="h4" sx={{ fontStyle: "italic" }}>
+                  {`"${quote.data.data.quote}"`}
+                </Typography>
+                <Typography variant="h4" sx={{ textAlign: "center", py: 2 }}>
+                  {`Author: "${quote.data.data.author}"`}
+                </Typography>
+              </Box>
+            </Box>
           </Box>
-        </Box>
+        </React.Fragment>
       )}
-    </React.Fragment>
+    </div>
   );
 };
 

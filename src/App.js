ddfd7b7d876685e416dc891dd1f53f987a7da32d
mamadoudiallo <mamadoudiallo@costco.com>
2022-04-13 11:26:42 -0700

    adding some global styling and testing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { ThemeProvider } from '@mui/material';
+import GlobalStyles from './components/GlobalStyles';
+
 import theme  from './theme'
 
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyles />
       <span style={{color: theme.palette.text.primary}}>Hello World</span>
     </ThemeProvider>
   );
